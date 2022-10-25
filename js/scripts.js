3e1208ee98acd8833d9718078c7128922e1f7622
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-10-25 16:47:31 -0600

    final13

--- js/scripts.js
+++ js/scripts.js
@@ -51,12 +51,12 @@ function getHotelDetailInfo(hotelID){
     method: 'GET',
     headers: {
         //'X-RapidAPI-Key': '5c80726bacmsha4026533a6e64d3p14eef0jsn0eb1adbcc357',
-        //'X-RapidAPI-Key': '6931d989ffmsh118bed85a85a6c4p176f43jsn2b460f96d581',
+        'X-RapidAPI-Key': '6931d989ffmsh118bed85a85a6c4p176f43jsn2b460f96d581',
         //'X-RapidAPI-Key': '04f2322d57msh40214398878f730p1baf7bjsnba45fd962457',
         //mia
         //'X-RapidAPI-Key': '7dc985f0bemsh074067a14f3282bp131014jsn8d3feb7f0864',
         //thecef02
-        'X-RapidAPI-Key': 'b858a9d176mshec01cd990f51108p1c807bjsn757d73457e96',
+        // 'X-RapidAPI-Key': 'b858a9d176mshec01cd990f51108p1c807bjsn757d73457e96',
         'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'}
 };
   const a = fetch(`https://hotels4.p.rapidapi.com/properties/get-details?id=${hotelID}&checkIn=2020-01-08&checkOut=2020-01-15&adults1=1&currency=USD&locale=en_US`, options)
@@ -79,7 +79,7 @@ function getHotelDetailInfo(hotelID){
 };
 
 function loadCombo(){
-  const requestFile = 'data/hotels.json';
+  const requestFile = './data/hotels.json';
   if ( document.URL.includes("index.html") ||  document.URL.includes("reservations.html") ){
     fetch(requestFile)
       .then(function (response) {
--- js/utils.js
+++ js/utils.js
@@ -31,8 +31,8 @@ export async function loadTemplate(path) {
 
 // load the header and footer
 export async function loadHeaderFooter() {
-    const header = await loadTemplate("../Final Project/partials/header.html");
-    const footer = await loadTemplate("../Final Project/partials/footer.html");
+    const header = await loadTemplate("./partials/header.html");
+    const footer = await loadTemplate("./partials/footer.html");
     const headerElement = document.getElementById("main-header");
     const footerElement = document.getElementById("main-footer");
     renderWithTemplate(header, headerElement);
