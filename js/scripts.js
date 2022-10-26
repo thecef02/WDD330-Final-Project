c96fb621fb84c342dfcf22003a83b49d111e51dd
Canyon Atwood <canyonatwood@gmail.com>
2022-10-25 18:54:22 -0600

    logo header and new API Key

--- js/scripts.js
+++ js/scripts.js
@@ -50,8 +50,11 @@ function getHotelDetailInfo(hotelID){
   const options = {
     method: 'GET',
     headers: {
+
+      'X-RapidAPI-Key': '5c80726bacmsha4026533a6e64d3p14eef0jsn0eb1adbcc357',
+    
         //'X-RapidAPI-Key': '5c80726bacmsha4026533a6e64d3p14eef0jsn0eb1adbcc357',
-        'X-RapidAPI-Key': '6931d989ffmsh118bed85a85a6c4p176f43jsn2b460f96d581',
+        //'X-RapidAPI-Key': '6931d989ffmsh118bed85a85a6c4p176f43jsn2b460f96d581',
         //'X-RapidAPI-Key': '04f2322d57msh40214398878f730p1baf7bjsnba45fd962457',
         //mia
         //'X-RapidAPI-Key': '7dc985f0bemsh074067a14f3282bp131014jsn8d3feb7f0864',
